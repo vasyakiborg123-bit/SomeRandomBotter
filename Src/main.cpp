c8b03b8883735c4e7afa609a5c365b785de6fe1b
vasyakiborg123-bit <vasyakiborg123@gmail.com>
2026-08-21 21:59:34 +0500

    Modify PlayLayer and PlayerObject for macro recording

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,15 +1,48 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include "macro.hpp"
+#include "global.hpp"
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, MenuLayer) 
-   $override
-   bool init() (
-      if (MenuLayer::init()) return false;
+class $modify(PlayLayer) {
+    void update(float dt) {
+        PlayLayer::update(dt);
+        
+        auto& g = Global::get();
+        
+        
+        if (g.state == state::playing) {
+            int frame = Global::getCurrentFrame();
+            
+        }
+    }
+};
 
+class $modify(PlayerObject) {
+    void pushButton(PlayerButton button) {
+        PlayerObject::pushButton(button);
+        
+        auto& g = Global::get();
+        if (g.state == state::recording) {
+            int frame = Global::getCurrentFrame();
+            bool p2 = this == PlayLayer::get()->m_player2;
+            Macro::recordAction(frame, static_cast<int>(button), p2, true);
+        }
+    }
+    
+    void releaseButton(PlayerButton button) {
+        PlayerObject::releaseButton(button);
+        
+        auto& g = Global::get();
+        if (g.state == state::recording) {
+            int frame = Global::getCurrentFrame();
+            bool p2 = this == PlayLayer::get()->m_player2;
+            Macro::recordAction(frame, static_cast<int>(button), p2, false);
+        }
+    }
+};
 
-
-      return true:
-    )
-        }}
+$execute {
+    
+    log::info("Macro mod loaded");
+}
