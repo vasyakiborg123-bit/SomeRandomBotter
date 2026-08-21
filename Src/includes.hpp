39c07a8805c8f6d0b6a4bf47ee657d19b0c98b5d
vasyakiborg123-bit <vasyakiborg123@gmail.com>
2026-08-21 22:18:18 +0500

    Add includes.hpp header file with necessary includes

---
+++ Src/includes.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <Geode/utils/VersionInfo.hpp>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <filesystem>
+#include <fstream>
