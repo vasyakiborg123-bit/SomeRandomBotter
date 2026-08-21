571356e4d74239e159e05d045faacfe1588f28a5
vasyakiborg123-bit <vasyakiborg123@gmail.com>
2026-08-21 22:34:51 +0500

    Add Geode headers to includes.hpp

--- Src/includes.hpp
+++ Src/includes.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
 #include <Geode/Geode.hpp>
 #include <Geode/utils/VersionInfo.hpp>
 #include <sstream>
